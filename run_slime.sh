e96c7070b1215a4a296990a95e87995d26f6f6d1
SAT-X <oscar.riveros@peqnp.science>
2022-04-21 12:23:29 -0400

    Update run_slime.sh

--- run_slime.sh
+++ run_slime.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
-mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 2 --hostfile $1 --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings slime $2
+time mpirun -quiet --mca btl_tcp_if_include eth0 --allow-run-as-root -np 2 --hostfile $1 --use-hwthread-cpus --map-by node:PE=2 --bind-to none --report-bindings slime $2 -sharing-clauses -alternate-sharing -filter=1000
